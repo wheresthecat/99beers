944d0e513212f2f9acb71b415f53710fafa8cfa8
Honza <frosty@centrum.cz>
2024-08-15 16:41:49 +0200

    Update 99beers.c
    Did a different version.

--- src/99beers.c
+++ src/99beers.c
@@ -4,14 +4,30 @@ int main(int argc, char **argv)
 {
     int beers = 99;
 
-    while(beers > 0)
+    while(1)
     {
-        printf("%d bottles of beer on the wall, %d bottles of beer.\n", beers, beers);
-        printf("Take one down, pass it around, %d bottles of beer on the wall.\n\n", beers-1);
-        beers--;
-
+        if(beers > 1)
+        {
+            printf("%d bottles of beer on the wall, %d bottles of beer.\n", beers, beers);
+            beers--;
+            if(beers > 1)
+            {
+                printf("Take one down, pass it around, %d bottles of beer on the wall.\n\n", beers);
+            }
+            else
+            {
+                printf("Take one down, pass it around, %d bottle of beer on the wall\n\n", beers);
+            }
+        }
+        else
+        {
+            printf("%d bottle of beer on the wall, 1 bottle of beer\n", beers);
+            printf("Take one down, pass it around, no more bottles of beer on the wall.\n\n");
+            break;
+        }
     }
 
+    printf("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n\n");
 
     return(0);
 }
